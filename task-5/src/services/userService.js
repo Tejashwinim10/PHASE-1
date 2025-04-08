5819944ea5ebc70ef8cf0c8dc91ee0c724096749
Tejashwinim10 <tejashwiniat123@gmail.com>
2025-04-08 15:28:31 +0530

    Delete task-5 directory

--- task-5/src/services/userService.js
+++
@@ -1,9 +0,0 @@
-export const submitUserData = (data) => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        console.log('User Data Submitted:', data);
-        resolve('Success');
-      }, 1500);
-    });
-  };
-  
